468d8cc439e6fba823ab380e02116ccd5cdbd198
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-20 23:03:59 +0600

    feat: implement product creation functionality - add POST / endpoint for creating a new product - validate incoming product data using Zod validation schema - integrage with ProductServices to strore product data in the database - return appropriate success or error responses based on the operation

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,19 +1,19 @@
-export type Variants = {
-  type: string;
-  value: string;
+export type TVariants = {
+    type: string;
+    value: string;
 }[];
 
-export type Inventory = {
-  quantity: number;
-  inStock: boolean;
+export type TInventory = {
+    quantity: number;
+    inStock: boolean;
 };
 
-export type Product = {
-  name: string;
-  description: string;
-  price: number;
-  category: string;
-  tags?: string[];
-  variants?: Variants;
-  inventory: Inventory;
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags?: string[];
+    variants?: TVariants;
+    inventory: TInventory;
 };
--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,60 +1,63 @@
-import mongoose from 'mongoose';
+import mongoose, { model } from 'mongoose';
+import { TProduct } from './product.interface';
 const { Schema } = mongoose;
 
 const variantsSchema = new Schema({
-  type: {
-    type: String,
-    required: true,
-  },
-  value: {
-    type: String,
-    required: true,
-  },
+    type: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: String,
+        required: true,
+    },
 });
 
 const inventorySchema = new Schema({
-  quantity: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  inStock: {
-    type: Boolean,
-    required: true,
-    default: true,
-  },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
 });
 
 export const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  tags: {
-    type: [String],
-    default: [],
-  },
-  variants: {
-    type: [variantsSchema],
-    default: [],
-  },
-  inventory: {
-    type: {
-      inventorySchema,
-      required: true,
+    name: {
+        type: String,
+        required: true,
     },
-  },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        default: [],
+    },
+    variants: {
+        type: [variantsSchema],
+        default: [],
+    },
+    inventory: {
+        type: {
+            inventorySchema,
+            required: true,
+        },
+    },
 });
+
+export const Product = model<TProduct>("Product", productSchema)
