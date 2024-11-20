8fae17b203638a96f846716ffb8049cc06fd0ab8
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-20 22:18:46 +0600

    feat: implement product schema - add product schema with fields for name, description, price, category, tags, variants, and inventory - schemas for variants and inventory with proper validation and default values

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const variantsSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: String,
+        required: true,
+    }
+})
+
+const inventorySchema = new Schema({
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+        default: true,
+    }
+})
+
+export const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    variants: {
+        type: [variantsSchema],
+        required: true,
+        default: [],
+    },
+    inventory: {
+        type: {
+            inventorySchema,
+            required: true,
+        }
+    },
+})
