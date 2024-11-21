1bf6a50456cc1bd2df9ddc7b3ad301bf24107077
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-21 09:56:12 +0600

    feat(api): add Get /products/:productId route to fetch a single product by ID

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -41,7 +41,26 @@ const getAllProducts = async (req: Request, res: Response) => {
 
 }
 
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.productId;
+    const result = await ProductServices.getSingleProductFromDB(productId)
+    res.status(200).json({
+      success: true,
+      message: "Successfully retreived a product",
+      data: result,
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message || "Internal server error",
+      error: err,
+    })
+  }
+}
+
 export const ProductControllers = {
   createProduct,
   getAllProducts,
+  getSingleProduct,
 };
